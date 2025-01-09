1b578acc1c51781e468cc94a25fb83a6c74c0597
Anupam Bista <anupambista3@gmail.com>
2025-01-09 14:05:09 +0545

    multi role authentication setup & register api completed

---
+++ server/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * Handle an incoming registration request.
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'role' => ['required', 'in:user,vendor'],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->string('password')),
+        ]);
+
+        $user->assignRole($request->role);
+
+        event(new Registered($user));
+
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $user = User::where('email', $request->email)->first();
+            $token = $user->createToken($user->name . '-AuthToken')->plainTextToken;
+
+            session()->regenerate();
+
+            return response()->json([
+                'user' => $user,
+                'access_token' => $token,
+                'token_type' => 'Bearer',
+                'role' => $user->getRoleNames()[0],
+            ]);
+        } else {
+
+            return response()->json([
+                'message' => 'Invalid Credentials'
+            ], 401);
+        }
+    }
+}
