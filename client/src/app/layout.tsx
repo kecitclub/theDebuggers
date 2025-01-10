32b7c70b99fbabd86b207be4ab12d9d6b4a5c68c
Nimesh <regminimesh7@gmail.com>
2025-01-10 21:24:36 +0545

    files organized

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -32,9 +32,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ToastContainer />
-        <SiteHeader />
+        {/* <SiteHeader /> */}
         {children}
-        <SiteFooter />
+        {/* <SiteFooter /> */}
       </body>
     </html>
   );
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,6 +2,8 @@ import { HeroSection } from "@/components/hero-section";
 import { ProjectCard } from "@/components/project-card";
 import { CategoriesSection } from "@/components/categories-section";
 import { TestimonialSection } from "@/components/testimonial-section";
+import { SiteFooter } from "@/components/site-footer";
+import { SiteHeader } from "@/components/site-header";
 
 export default function Home() {
   const projects = [
@@ -31,20 +33,24 @@ export default function Home() {
   ];
 
   return (
-    <main className="">
-      <HeroSection />
-      <section className=" md:px-6 py-12 max-w-7xl mx-auto px-2">
-        <h2 className="text-2xl font-bold mb-8 text-center">
-          Trending Projects
-        </h2>
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {projects.map((project, i) => (
-            <ProjectCard key={i} {...project} />
-          ))}
-        </div>
-      </section>
-      <CategoriesSection />
-      <TestimonialSection />
-    </main>
+    <>
+    <SiteHeader />
+      <main className="">
+        <HeroSection />
+        <section className=" md:px-6 py-12 max-w-7xl mx-auto px-2">
+          <h2 className="text-2xl font-bold mb-8 text-center">
+            Trending Projects
+          </h2>
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {projects.map((project, i) => (
+              <ProjectCard key={i} {...project} />
+            ))}
+          </div>
+        </section>
+        <CategoriesSection />
+        <TestimonialSection />
+      </main>
+      <SiteFooter />
+    </>
   );
 }
