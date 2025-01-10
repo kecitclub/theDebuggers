1c2f3bdcc1d534ce5cffac6154cf08e158c06c0e
Anupam Bista <anupambista3@gmail.com>
2025-01-11 00:23:06 +0545

    category api integration

--- client/src/components/categories-section.tsx
+++ client/src/components/categories-section.tsx
@@ -1,35 +1,37 @@
-import axios from "axios";
-import { Leaf, Heart, GraduationCap, Droplets } from "lucide-react";
+"use client";
+
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { categories } from "@/lib/home";
 
-
-const categoriesData=async()=>{
-  const response=await axios.get(`/categories/`);
-  console.log(response);
-  return <></>
-}
 export function CategoriesSection() {
-  const categories = [
-    { icon: Leaf, label: "Environment" },
-    { icon: Heart, label: "Health" },
-    { icon: GraduationCap, label: "Education" },
-    { icon: Droplets, label: "Water" },
-  ];
+  const [serverCategories, setServerCategories] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await categories();
+      setServerCategories(response);
+    };
+    fetchData();
+  }, []);
 
   return (
     <section className="py-12 bg-gray-50 max-w-7xl mx-auto px-2">
       <div className="px-4 md:px-6">
         <h2 className="text-2xl font-bold text-center mb-8">Categories</h2>
         <div className="grid grid-cols-2 md:grid-cols-4 gap-8">
-          {categories.map((category, index) => (
+          {serverCategories.map((category, index) => (
             <div
               key={index}
               className="flex flex-col items-center space-y-2 cursor-pointer"
             >
-              <Link href={`/category/${category.label}`} className="p-4 rounded-full bg-green-100">
-                <category.icon className="h-6 w-6 text-green-600" />
+              <Link
+                href={`/category/${category.id}`}
+                className="p-4 rounded-full bg-green-100"
+              >
+                {/* <category.icon className="h-6 w-6 text-green-600" /> */}
               </Link>
-              <span className="text-sm font-medium">{category.label}</span>
+              <span className="text-sm font-medium">{category.name}</span>
             </div>
           ))}
         </div>
